b5134a1fc33c794d32d8e98f0ce151527f54f808
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-05-04 14:01:44 -0300

    add: adicionado arquivo com exercícios de funções.

---
+++ exercicios-funcoes.js
@@ -0,0 +1,74 @@
+// Lista de exercícios sobre funções em JavaScript
+
+/*
+1. Crie uma função chamada `saudar` que recebe um nome como parâmetro
+   e retorna a mensagem: "Olá, <nome>! Seja bem-vindo(a)!".
+*/
+
+
+/*
+2. Crie uma função chamada `somar` que recebe dois números e retorna a soma.
+   Teste a função com valores diferentes.
+*/
+
+
+/*
+3. Crie uma função chamada `estaPar` que recebe um número e retorna `true`
+   se ele for par, ou `false` caso contrário.
+*/
+
+
+/*
+4. Crie uma função chamada `areaRetangulo` que recebe a largura e a altura de um
+   retângulo e retorna a área.
+*/
+
+
+/*
+5. Crie uma função chamada `calcularMedia` que recebe um array de números e
+   retorna a média desses valores.
+*/
+
+
+/*
+6. Crie uma função chamada `maiorNumero` que recebe um array de números e retorna
+   o maior valor presente no array.
+*/
+
+
+/*
+7. Crie uma função chamada `multiplicarElementos` que recebe um array e um número.
+   A função deve retornar um novo array com cada elemento multiplicado pelo número.
+*/
+
+
+/*
+8. Crie uma função chamada `contadorDeVogais` que recebe uma string e retorna o
+   número de vogais encontradas na palavra ou frase.
+*/
+
+
+/*
+9. Crie uma função chamada `calcularIMC` que recebe peso (kg) e altura (m)
+   e retorna o índice de massa corporal (IMC) com duas casas decimais.
+*/
+
+
+/*
+10. Crie uma função chamada `validarSenha` que recebe uma senha e retorna `true`
+    se ela tiver pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula
+    e um número. Caso contrário, retorna `false`.
+*/
+
+
+// Exemplos de uso das funções
+console.log(saudar('Aluno'));
+console.log(somar(5, 7));
+console.log(estaPar(10));
+console.log(areaRetangulo(4, 5));
+console.log(calcularMedia([8, 7, 9]));
+console.log(maiorNumero([3, 10, 6]));
+console.log(multiplicarElementos([1, 2, 3], 2));
+console.log(contadorDeVogais('Desenvolvimento')); 
+console.log(calcularIMC(70, 1.75));
+console.log(validarSenha('Senha123'));
