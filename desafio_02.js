1d818166ea79e4aca508fa71e828005a1c8e28b6
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-05-07 16:16:26 -0300

    add: adicionado desafio 02

---
+++ desafio_02.js
@@ -0,0 +1,9 @@
+/*
+Você está desenvolvendo uma API para uma indústria que monitora a temperatura de máquinas durante o 
+funcionamento da fábrica. Os sensores enviam leituras de temperatura constantemente para o sistema. 
+
+Porém:
+    - Algumas leituras chegam inválidas
+    - A equipe de manutenção precisa analisar rapidamente os dados
+    - Temperaturas muito altas podem indicar risco de falha
+*/
